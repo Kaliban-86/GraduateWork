ec56094d17fc0a64cec74d60e264b16a1c48549c
koxan <koxanovskij86@mail.ru>
2022-11-02 23:35:26 +0300

    Организовано чтение файла в формате DICOM

--- read_DICOM.py
+++ read_DICOM.py
@@ -2,9 +2,21 @@ import os
 import pydicom
 import numpy
 
+# записываем в переменную путь к файлам
+dir = 'to_read_DICOM'
 
-'''
-не обходимо прочитать файл и перевести его в массив  
+# смотрим список файлов в директории с файлами DICOM
+file_list = os.listdir(dir)
 
-'''
+# выводим длинну списка файлов
+print(len(file_list))
 
+# выводим первый элемен
+print(file_list[0])
+
+# загружаем первый файл для изучения и смотрим его тип
+ds = pydicom.read_file(f'{dir}/{file_list[0]}')
+print(type(ds))
+
+print(ds)
+print(ds.Modality)
